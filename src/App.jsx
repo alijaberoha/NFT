e67d4b293ae471e4e3d265ec10cce64133997261
Ali <alijaberoha@hotmail.com>
2025-05-07 15:21:25 +0200

    end

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,33 @@ function App() {
 
   return (
     <>
-      
+      <div className='box'>
+        <div className='image'>
+          <img src="public/images/image-equilibrium.jpg" alt="" />
+        </div>
+        <div className='titre'>
+          <h1>Equilibrium #3429</h1>
+        </div>
+        <div className='texte'>
+          <p>Our Equilibrium collection promotes balance and calm.</p>
+        </div>
+      <div className='info'>
+        <div className='vert'> 
+          <img src="public/images/icon-ethereum.svg" alt="" />
+          <p>0.041ETH</p>
+        </div>
+        <div className='gris'> 
+          <img src="public/images/icon-clock.svg" alt="" />
+          <p> 3 days left </p>
+        </div>
+      </div>
+      <div className='ligne'>
+      </div>
+      <div className='footer'> 
+          <img src="public/images/image-avatar.png" alt="" />
+          <p> Creation of  <span> Jules Wyvern</span> </p>
+        </div>
+      </div>
     </>
   )
 }
