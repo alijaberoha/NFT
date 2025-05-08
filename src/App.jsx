d5a3862a2c383605002c55aac46d546699932658
Ali <alijaberoha@hotmail.com>
2025-05-08 10:34:44 +0200

    last

--- src/App.jsx
+++ src/App.jsx
@@ -38,3 +38,37 @@ function App() {
 }
 
 export default App
+{/* <div className="box">
+  <div className="image-container">
+    <img src="images/image-equilibrium.jpg" alt="NFT" className="main-image" />
+    <div className="hover-layer">
+      <img src="images/icon-view.svg" alt="view icon" className="view-icon" />
+    </div>
+  </div>
+
+  <div className="titre">
+    <h1>Equilibrium #3429</h1>
+  </div>
+
+  <div className="texte">
+    <p>Our Equilibrium collection promotes balance and calm.</p>
+  </div>
+
+  <div className="info">
+    <div className="vert">
+      <img src="images/icon-ethereum.svg" alt="eth" />
+      <p>0.041 ETH</p>
+    </div>
+    <div className="gris">
+      <img src="images/icon-clock.svg" alt="clock" />
+      <p className="hover-blue">3 days left</p>
+    </div>
+  </div>
+
+  <div className="ligne"></div>
+
+  <div className="footer">
+    <img src="images/image-avatar.png" alt="avatar" />
+    <p>Creation of <span className="hover-blue">Jules Wyvern</span></p>
+  </div>
+</div> */}
